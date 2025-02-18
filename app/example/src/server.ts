865f3b3df8cf1d203af3a10c597d0bc43c02ff44
Yelyzaveta Rohovska <lizarogovskaia@gmail.com>
2025-02-18 14:40:38 +0100

    init setup commit

---
+++ app/example/src/server.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { api } from "./api";
+
+export const server = api.listen(api.get("port"), api.get("host"), () => {
+    console.log(
+        "  App is running at http://%s:%d in %s mode",
+        api.get("host"),
+        api.get("port"),
+        api.get("env"),
+    );
+});
